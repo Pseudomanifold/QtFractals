2bb4bf2cb6dc3f9d14990b814ab29d39634861a1
Bastian Rieck <bastian@rieck.ru>
2014-06-15 21:05:55 +0200

    First working shader

--- RenderWidget.cc
+++ RenderWidget.cc
@@ -18,12 +18,26 @@ void RenderWidget::initializeGL()
 
   glEnable(GL_DEPTH_TEST);
 
+  _shaderProgram.addShaderFromSourceCode(QGLShader::Vertex,
+                                         "attribute highp vec4 vertex;\n"
+                                         "void main()\n"
+                                         "{\n"
+                                         "  gl_Position = vertex;\n"
+                                         "}\n");
+
+
+
   bool fragmentShaderAdded
     = _shaderProgram.addShaderFromSourceFile(QGLShader::Fragment,
                                              ":/Mandelbrot.glsl");
 
   if(!fragmentShaderAdded)
     qDebug() << _shaderProgram.log();
+
+  _shaderProgram.link();
+  _shaderProgram.bind();
+
+  qDebug() << _shaderProgram.log();
 }
 
 void RenderWidget::resizeGL(int w, int h)
@@ -34,4 +48,19 @@ void RenderWidget::resizeGL(int w, int h)
 void RenderWidget::paintGL()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    GLfloat const quadVertices[] =
+  {
+    -1.f, -1.f,
+    1.f, -1.f,
+    1.f, 1.f,
+    -1.f, 1.f
+  };
+
+  int vertexLocation = _shaderProgram.attributeLocation("vertex");
+
+  _shaderProgram.enableAttributeArray(vertexLocation);
+  _shaderProgram.setAttributeArray(vertexLocation, quadVertices, 2);
+
+  glDrawArrays(GL_QUADS, 0, 4);
 }
