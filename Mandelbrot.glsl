0701ba14f10e2471df711b7a6407a59162e7114d
Bastian Rieck <bastian@rieck.ru>
2014-06-15 21:30:11 +0200

    Non-functioning texture coordinates

--- Mandelbrot.glsl
+++ Mandelbrot.glsl
@@ -1,4 +1,11 @@
+uniform int iterations;
+
 void main()
 {
-  gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+  vec2 position  = gl_FragCoord.xy; 
+  
+  if(position.x <= 0.0 && position.y < 1.0)
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 0.0);
+  else
+    gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
 }
--- RenderWidget.cc
+++ RenderWidget.cc
@@ -19,13 +19,11 @@ void RenderWidget::initializeGL()
   glEnable(GL_DEPTH_TEST);
 
   _shaderProgram.addShaderFromSourceCode(QGLShader::Vertex,
-                                         "attribute highp vec4 vertex;\n"
+                                         "attribute highp vec2 vertex;\n"
                                          "void main()\n"
                                          "{\n"
-                                         "  gl_Position = vertex;\n"
+                                         "  gl_Position = vec4(vertex.xy, 0.0, 0.0);\n"
                                          "}\n");
-
-
 
   bool fragmentShaderAdded
     = _shaderProgram.addShaderFromSourceFile(QGLShader::Fragment,
@@ -49,7 +47,7 @@ void RenderWidget::paintGL()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    GLfloat const quadVertices[] =
+  const GLfloat quadVertices[] =
   {
     -1.f, -1.f,
     1.f, -1.f,
@@ -57,10 +55,22 @@ void RenderWidget::paintGL()
     -1.f, 1.f
   };
 
-  int vertexLocation = _shaderProgram.attributeLocation("vertex");
+  const GLfloat textureCoordinates[] =
+  {
+    0.f, 0.f,
+    1.f, 0.f,
+    1.f, 1.f,
+    0.f, 1.f
+  };
+
+  int vertexLocation  = _shaderProgram.attributeLocation("vertex");
+  int textureLocation = _shaderProgram.attributeLocation("texture");
 
   _shaderProgram.enableAttributeArray(vertexLocation);
   _shaderProgram.setAttributeArray(vertexLocation, quadVertices, 2);
 
   glDrawArrays(GL_QUADS, 0, 4);
+
+  _shaderProgram.enableAttributeArray(textureLocation);
+  _shaderProgram.setAttributeArray(textureLocation, textureCoordinates, 2);
 }
