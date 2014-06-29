07428bd9896860ca0ee60236846b2a83bcc1bd45
Bastian Rieck <bastian@rieck.ru>
2014-06-29 19:47:24 +0200

    Added code for Julia sets

---
+++ Julia.glsl
@@ -0,0 +1,40 @@
+uniform int iterations;
+
+uniform highp vec2 centre;
+uniform highp float scale;
+
+varying highp vec2 texture_out;
+
+void main()
+{
+  vec2 z;
+  vec2 c;
+  c.x = ( texture_out.x - 0.5 ) + centre.x;
+  c.y = ( texture_out.y - 0.5 ) + centre.y;
+
+  z = c;
+
+  int i = 0;
+  for(; i < iterations; ++i)
+  {
+    float x = z.x*z.x - z.y*z.y + c.x;
+    float y = z.x*z.y + z.y*z.x + c.y;
+
+    if( x*x + y*y > 4.0 )
+      break;
+
+    z.x = x;
+    z.y = y;
+  }
+
+  vec4 color = vec4(0.0);
+
+  if(i < iterations - 1)
+  {
+    color.x = sin(float(i) / 3.0);
+    color.y = sin(float(i) / 6.0);
+    color.z = cos(float(i) / 12.0 + 3.141 / 4.0);
+  }
+
+  gl_FragColor = color;
+}
